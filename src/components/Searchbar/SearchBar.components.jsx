61fc88efdf0cfc91e2ad0bf3efd1e73f67d0a10a
siddhantprateek <siddhantprateek@gmail.com>
2022-04-07 23:33:29 +0530

    🚩Search Bar Features added

--- src/components/Searchbar/SearchBar.components.jsx
+++ src/components/Searchbar/SearchBar.components.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
-import './Searchbar.styles.css'
-import { SearchIcon } from '../../assets'
+import React from "react";
+import "./Searchbar.styles.css";
+import { SearchIcon } from "../../assets";
 function SearchBar() {
   return (
-    <div className='search-bar-section'>
-        <div className="search-bar">
+    <div className="search-bar-section">
+      <div className="search-bar">
+        <div className="search-option">
           <h3>Location</h3>
-          <h3>Check In</h3>
-          <h3>Check Out</h3>
-          <h3>Guests</h3>
-          <img src={SearchIcon} alt="" />
+          <input placeholder="Where are you Going?" />
         </div>
+        <div className="search-option">
+          <h3>Check In</h3>
+          <input placeholder="Add dates" />
+        </div>
+        <div className="search-option">
+          <h3>Check Out</h3>
+          <input placeholder="Add dates" />
+        </div>
+        <div className="search-option">
+          <h3>Guests</h3>
+          <input placeholder="Add guests" />
+        </div>
+        <img src={SearchIcon} className="search-icon" alt="" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default SearchBar
+export default SearchBar;
