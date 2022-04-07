81793f6b847fe7559377826779aee9c97331b173
siddhantprateek <siddhantprateek@gmail.com>
2022-04-07 12:00:03 +0530

    🚩Other configuration for UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { Home, Explore, Review } from './routes' 
+import { Header, HeaderDark } from './components';
 
 function App() {
+  const location = useLocation()
+  console.log(location.pathname)
   return (
     <div className="App">
-      <h1>COOL</h1>
+      {location.pathname === "/" ? <Header/> : <HeaderDark/>}
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/explore" element={<Explore/>}/>
+        <Route path="/hotel/review" element={<Review/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Header } from './Header/Header.components'
+export { default as HeaderDark } from './HeaderDark/HeaderDark.components'
+
+export { default as Footer } from './Footer/Footer.components'
+export { default as SearchBar } from './Searchbar/SearchBar.components'
