6cb0aaeb3826bb2d76cc76e69378aebeb460b954
siddhantprateek <siddhantprateek@gmail.com>
2022-04-13 10:23:37 +0530

    🚩Tour packages options

---
+++ src/routes/Tour/Tour.routes.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { TourCard } from '../../components'
+import './Tour.styles.css'
+function Tour() {
+  return (
+    <div className='tour-page'>
+      <TourCard/>
+      <TourCard/>
+      <TourCard/>
+      <TourCard/>
+      <TourCard/>
+      <TourCard/>
+    </div>
+  )
+}
+
+export default Tour
